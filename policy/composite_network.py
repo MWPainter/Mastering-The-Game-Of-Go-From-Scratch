c77663f650c749958e7eadecb57829a9a68f89b1
Michael Painter <michaelpainter.1994@gmail.com>
2017-06-10 15:38:17 -0700

    Added composite network interface

---
+++ policy/composite_network.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+class CompositeNetwork(BasicNetwork):
+
+    def __init__(self, sub_network_filename, num_filters_global, num_new_filter, 
+                 board_size, config, logger):
+        pass
+
+    # stuff also in basic_network.py
+
+
